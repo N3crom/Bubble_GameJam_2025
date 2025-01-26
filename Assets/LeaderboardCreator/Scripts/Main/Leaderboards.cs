bc5d725955ce7f08b1be4adba739a3402bf92483
Furnok <killian.furno@orange.fr>
2025-01-26 10:20:51 +0100

    Feat: Leaderboard

---
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,7 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference Score = new LeaderboardReference("ccc818cec2fc6321c29c9632b64a2aea7c25c725e4230a93f91fa3afb3c08321");
+    }
+}
---
+++ Assets/Scripts/Leaderboard/Leaderboard.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Dan.Main;
+using TMPro;
+using System.Runtime.CompilerServices;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+using static UnityEditor.Progress;
+using System;
+
+public class Leaderboard : MonoBehaviour
+{
+    [SerializeField] 
+    private List<TextMeshProUGUI> textRank;
+    [SerializeField] 
+    private List<TextMeshProUGUI> textName;
+    [SerializeField] 
+    private List<TextMeshProUGUI> textWave;
+
+    public GameObject scroll;
+
+    private void Start()
+    {
+        //UploadEntry();
+        LoadEntries();
+    }
+
+    public void LoadEntries()
+    {
+        Leaderboards.Score.GetEntries((entries) =>
+        {
+            int value = 1;
+
+            foreach (var t in textRank)
+            {
+                t.text = value.ToString() + ".";
+                value++;
+            }
+
+            foreach (var t in textName)
+            {
+                t.text = "EMPTY";
+            }
+
+            foreach (var t in textWave)
+            {
+                t.text = "0";
+            }
+
+            int loopLength = (entries.Length < textName.Count) ? entries.Length : textName.Count;
+            for (int i = 0; i < loopLength; i++)
+            {
+                bool isMine = entries[i].IsMine();
+
+                if(isMine)
+                {
+                    textRank[i].color = Color.green;
+                    textName[i].color = Color.green;
+                    textWave[i].color = Color.green;
+                }
+
+                textRank[i].text = $"{entries[i].Rank}" + ".";
+                textName[i].text = $"{entries[i].Username}";
+                textWave[i].text = $"{entries[i].Score}";
+            }
+        });
+    }
+
+    public void UploadEntry()
+    {
+        Leaderboards.Score.UploadNewEntry("Paul", 50);
+        Leaderboards.Score.ResetPlayer();
+    }
+}
