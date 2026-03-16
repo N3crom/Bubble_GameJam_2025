779c9a0e3e102333451f1ce26739660ce9adb93e
Killian FURNO <killian.furno@orange.fr>
2026-03-16 16:44:52 +0100

    Fix: Project Clean Up

---
+++ Assets/App/Scripts/Leaderboard/S_LeaderboardShow.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class S_LeaderboardShow : MonoBehaviour
+{
+    [SerializeField] private List<TextMeshProUGUI> textRank;
+    [SerializeField] private List<TextMeshProUGUI> textName;
+    [SerializeField] private List<TextMeshProUGUI> textScore;
+
+    private void Start()
+    {
+        LoadEntries();
+    }
+
+    public void LoadEntries()
+    {
+        /*Leaderboards.Scores.GetEntries((entries) =>
+        {
+            int value = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                textRank[i].text = value.ToString();
+                textName[i].text = "EMPTY";
+                textScore[i].text = "0";
+
+                value++;
+            }
+
+            int loopLength = (entries.Length < textName.Count) ? entries.Length : textName.Count;
+
+            for (int i = 0; i < loopLength; i++)
+            {
+                textRank[i].text = $"{entries[i].Rank}";
+                textName[i].text = $"{entries[i].Username}";
+                textScore[i].text = $"{entries[i].Score}";
+            }
+        });*/
+    }
+}
